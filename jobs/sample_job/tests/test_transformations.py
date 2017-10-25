f985e22c6d5e1f50bb398eb6b626bdeb2b65188b
Gary Lucas <lucas.gary@Gmail.com>
2017-10-24 21:35:33 -0700

    first functional test

--- jobs/sample_job/tests/test_transformations.py
+++ jobs/sample_job/tests/test_transformations.py
@@ -1,27 +1,47 @@
 from jobs.sample_job import transformations
 from pyspark.sql import SparkSession
-from sparktestingbase.testcase import SparkTestingBaseTestCase
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from sparktestingbase.sqltestcase import SQLTestCase
 
-df1_fixture = [{"value": "some_value", "id": 1},
-               {"value": "some_other_value", "id": 2}]
+df1_schema = StructType([
+    StructField("id", IntegerType(), True),
+    StructField("value", StringType(), True)
+])
+
+df1_fixture = [(1, "some_value"),
+               (2, "some_other_value")]
+
+df2_schema = StructType([
+    StructField("id", IntegerType(), True),
+    StructField("name", StringType(), True)
+])
+
 df2_fixture = [
-    {"name": "derp", "id": 1},
-    {"name": "non_derp", "id": 2},
-    {"name": "missing_Derp", "id": 3}
+    (1, "derp"),
+    (2, "non_derp"),
+    (3, "missing_derp")
 ]
 
+df_expected_schema = StructType([
+    StructField("id", IntegerType(), True),
+    StructField("name", StringType(), True),
+    StructField("value", StringType(), True)]
+)
 
 df_expected_fixture = \
-    [{"value": "some_value", "name": "derp", "id": 1},
-     {"value": "some_other_value", "name": "non_derp", "id": 2}]
+    [(1, "derp", "some_value"),
+     (2, "non_derp", "some_other_value")]
 
-class HelloWorldTest(SparkTestingBaseTestCase):
+
+class HelloWorldTest(SQLTestCase):
     def test_join_by_ids(self):
         spark_session = SparkSession(self.sc)
-        df1 = spark_session.read.json(self.sc.parallelize(df1_fixture))
-        df2 = spark_session.read.json(self.sc.parallelize(df2_fixture))
-        df_expected = spark_session.read(self.sc.parallelize(df_expected_fixture))
-        x = transformations.join_by_ids(self.sc, df1, df2)
-        x.show()
+        df1 = spark_session.createDataFrame(df1_fixture, df1_schema)
+        df2 = spark_session.createDataFrame(df2_fixture, df2_schema)
+
+        df_expected = spark_session.createDataFrame(df_expected_fixture, df_expected_schema)
         df_expected.show()
-        SparkTestingBaseTestCase.assertEqual(self, x, df_expected, "dfs don't match")
+
+        result = transformations.join_by_ids(self.sc, df2, df1)
+        result.show()
+        SQLTestCase.assertDataFrameEqual(self, result, df_expected, "dfs don't match")
